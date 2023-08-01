65eb6f3e5d391a4c71d554f1d677de1c13fa144c
Will <billy.bowes@yo61.com>
2023-08-01 14:00:29 +0100

    Add files via upload

---
+++ click.cpp
@@ -0,0 +1,44 @@
+#include <Windows.h>
+#include <stdlib.h>
+#include <iostream>
+
+int main() {
+    int lmb_cps, rmb_cps;
+    bool toggle = true;
+
+    std::cout << "Left click CPS: ";
+    std::cin >> lmb_cps;
+
+    if(lmb_cps >= 20 || lmb_cps < 1) { main(); }
+
+    system("cls");
+
+    std::cout << "Toggle: F4" << std::endl;
+    std::cout << "Left CPS: ";
+    std::cout << lmb_cps << std::endl;
+
+    bool clicking, in_focus;
+    HWND window;
+    int left_delay = (2000 / lmb_cps) / 1;
+    int right_delay = (2000 / rmb_cps) / 1;
+
+    while(true) {
+
+        window = GetForegroundWindow();
+        clicking = GetAsyncKeyState(VK_LBUTTON);
+        in_focus = FindWindowA(("LWJGL"), nullptr) == window;
+
+        if(toggle && clicking && in_focus) {
+            SendMessageW(window, WM_LBUTTONDOWN, MK_LBUTTON, MAKELPARAM(0, 0));
+            SendMessageW(window, WM_LBUTTONUP, MK_LBUTTON, MAKELPARAM(0, 0));
+            Sleep(rand() % left_delay);
+        }
+
+        if(GetAsyncKeyState(VK_F4)) {
+            toggle = !toggle;
+            Sleep(200);
+        }
+    }
+
+
+}
